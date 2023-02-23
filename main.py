f6abb3e622d6076533b038a428c432b03724e88c
duckduckgrayduck <102841251+duckduckgrayduck@users.noreply.github.com>
2023-02-23 15:28:00 -0600

    Adding f strings

--- main.py
+++ main.py
@@ -10,8 +10,8 @@ class Reflow(SoftTimeOutAddOn):
     def main(self):
         self.set_message("Starting to re-flow documents...")
         for document in self.get_documents():
-            self.set_message("Reflowing {document.title}...")
-            process = Popen(["k2pdfopt {document.title}.pdf -w {height} -h {width} -dpi {ppi} -idpi -2 -x"], stdin=PIPE, shell=True)
+            self.set_message(f"Reflowing {document.title}...")
+            process = Popen([f"k2pdfopt {document.title}.pdf -w {height} -h {width} -dpi {ppi} -idpi -2 -x"], stdin=PIPE, shell=True)
             process.communicate(input='\n'.encode('utf-8'))
             self.set_message("Uploading reflowed PDF")
             self.client.documents.upload(f"{document.title}_k2opt.pdf")
