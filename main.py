ff91ebcaaca337156430765cef7045de4ae1eed1
duckduckgrayduck <102841251+duckduckgrayduck@users.noreply.github.com>
2023-02-23 16:31:19 -0600

    Update main.py

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ class Reflow(SoftTimeOutAddOn):
             with open(pdf_name, "wb") as file:
                 file.write(document.pdf)
             self.set_message(f"Reflowing {document.title}...")
-            process = Popen([f"k2pdfopt {document.title}.pdf -w {height} -h {width} -dpi {dpi} -idpi -2 -x"], stdin=PIPE, stdout=open(os.devnull, 'w'), shell=True)
+            process = Popen([f"k2pdfopt {document.title}.pdf -w {height} -h {width} -dpi {dpi} -idpi -2 -x"], stdin=PIPE, shell=True)
             process.communicate(input='\n'.encode('utf-8'))
             self.set_message("Uploading reflowed PDF")
             self.client.documents.upload(f"{document.title}_k2opt.pdf")
